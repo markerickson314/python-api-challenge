702f1d49100f5366c3fa3ad922ccdf01d6a837b4
Mark Erickson <mje314@Marks-MacBook-Air.local>
2020-07-17 17:01:45 -0500

    created csv of city weather data

--- WeatherPy/api_keys.py
+++ WeatherPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "fb5d392c812876dd00db33ef87fac3c8"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyBYEIf5GrhkA1n359sCIh6uv1HzMzmNmBM"
